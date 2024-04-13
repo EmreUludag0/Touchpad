669c4a54d8da2b56c19dff9a95089368404c60c3
EmreUludag0 <155458894+EmreUludag0@users.noreply.github.com>
2024-04-13 21:56:04 +0300

    2024

--- main.py
+++ main.py
@@ -3,31 +3,30 @@ import mediapipe as mp
 
 video = cv2.VideoCapture(0)
 
-yukseklik_esik = 50
 
 mp_cizim = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
+
+yukseklik_esik = 0.5 #0.5'ten buyukse parmak olarak kabul edilir
+
 
 with mp_hands.Hands(static_image_mode=False) as  hands:
     while True:
         kontrol, kamera = video.read()
 
-        sonuc = hands.process(cv2.cvtColor(kamera, cv2.COLOR_BGR2RGB))
+        rgb = cv2.cvtColor(kamera, cv2.COLOR_BGR2RGB)
+        sonuc = hands.process(rgb)
 
+        # parmak index atama kısmı
         if sonuc.multi_hand_landmarks:
             for hand_landmarks in sonuc.multi_hand_landmarks:
-                yukseklikler = []
-                for landmark in hand_landmarks:
-                    yukseklikler.append(landmark.z)
+                yukseklikler = [landmark.y for landmark in hand_landmarks.landmark] #normalleştirilmiş yukseklik listesi                
                 
-            parmakSayisi = 0
-            for yukseklik in yukseklikler:
-                if yukseklik > yukseklik_esik:
-                    parmakSayisi += 1
+                parmakSayisi = sum(y > yukseklik_esik for y in yukseklikler)
+                
+                print("parmak sayisi", parmakSayisi)
 
-            print("parmak sayisi", parmakSayisi)
-
-            mp_cizim.draw_landmarks(kamera, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                mp_cizim.draw_landmarks(kamera, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         cv2.imshow('El Cizimi',kamera)        
 
