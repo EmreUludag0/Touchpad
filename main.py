b71c446dce1dbf2601a71055cbb2542d48d9b093
EmreUludag0 <155458894+EmreUludag0@users.noreply.github.com>
2024-04-15 12:53:45 +0300

    2024

--- main.py
+++ main.py
@@ -2,14 +2,33 @@ import cv2
 import mediapipe as mp
 import time
 import ctypes
+import pyautogui
+import win32gui
+import win32process
+import win32con
 
 video = cv2.VideoCapture(0)
-
-
 mp_cizim = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+
 yukseklik_esik = 0.5 #0.5'ten buyukse parmak olarak kabul edilir
+
+
+windows = pyautogui.getAllTitles()
+
+def Windows():
+    for window in windows:
+        if window != "Masaüstü":  
+            pyautogui.getWindowsWithTitle(window)[0].minimize()  # Pencereyi minimize et
+            time.sleep(0.5)
+
+def yeniMasaustu():
+    new_desktop = "MyNewDesktop"
+    win32gui.CreateDesktop(new_desktop, None, None, 0, win32con.DESKTOP_CREATEWINDOW, None)
+    win32gui.SwitchDesktop(win32gui.OpenDesktop(new_desktop, 0, False, win32con.DESKTOP_SWITCHDESKTOP))
+
+    
 
 
 with mp_hands.Hands(static_image_mode=False) as  hands:
@@ -41,15 +60,15 @@ with mp_hands.Hands(static_image_mode=False) as  hands:
         komut5 = 9
 
         
-        time.sleep(2)
+        #time.sleep(2)
         if(yedekParmakSayisi == komut1):
             ctypes.windll.user32.LockWorkStation()
             break
         elif(yedekParmakSayisi == komut2):
-            
+            Windows()
             break
         elif(yedekParmakSayisi == komut3):
-            print("3 parmak gösteriliyor")
+            yeniMasaustu()
             break
         elif(yedekParmakSayisi == komut4):
             print("4 parmak gösteriliyor")
